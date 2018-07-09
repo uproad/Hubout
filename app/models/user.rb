be69a7af629ca629b9a064685e0efd743d3b8762
uproad krowd <7349115+uproad@users.noreply.github.com>
2018-07-09 17:16:49 +0900

    refs #5 [add] User.nameに関するデータ入力項目を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  validates :name, :presence => true, :uniqueness => true, length: { maximum: 16 }, format: { with: /\A[A-Za-z0-9]+\z/i }
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
