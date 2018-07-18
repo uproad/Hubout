e1ddc1aa1b89c670ff30a2ff28d4b3a4f7b3789c
uproad krowd <7349115+uproad@users.noreply.github.com>
2018-07-18 17:12:45 +0900

    refs #11 [command] rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_09_053932) do
+ActiveRecord::Schema.define(version: 2018_07_18_075512) do
+
+  create_table "trpg_systems", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
